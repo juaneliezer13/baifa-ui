3c483ecdbb0a38508876ff95e90869a336987dbb
Juan Chirinos <juaneliezer13@gmail.com>
2026-09-09 13:55:43 -0400

    fix(config): activar prefixComposables en vuetify-nuxt-module para evitar colision con useLayout

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,4 @@
-﻿// https://nuxt.com/docs/api/configuration/nuxt-config
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
@@ -30,7 +30,7 @@ export default defineNuxtConfig({
 
   vuetify: {
     moduleOptions: {
-      /* opciones específicas del módulo */
+      prefixComposables: true
     },
     vuetifyOptions: {
       theme: {
